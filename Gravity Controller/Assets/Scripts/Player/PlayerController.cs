513f79a0990489efb06c49063ccbd624e48db6d0
KimGwangmin <gwangmin@snu.ac.kr>
2024-10-30 20:39:51 +0900

    feat: add global gravity control(lower)
    add global gravity control: lower gravity for player when mouse wheel up input

--- Gravity Controller/Assets/Scripts/Player/PlayerController.cs
+++ Gravity Controller/Assets/Scripts/Player/PlayerController.cs
@@ -1,14 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    //
+    private Rigidbody _rigidbody;
+
+    [SerializeField] Image _energyBar;
+
+    // player status
     public int hp;
+    [SerializeField] private float _energy;
+    [SerializeField] private float _maxEnergy;
+    [SerializeField] private float _energyRecover;
     private bool _isAlive;
 
+    // to set direction of raycast
     private Transform _camera;
-
     // fire gun(left click)
     private bool _isShootable;
     private bool _isReloading;
@@ -21,20 +34,37 @@ public class PlayerController : MonoBehaviour
     // local gravity control(right click)
     private bool _isTargetable;
     [SerializeField] private float _targetGravityCooldown;
-    [SerializeField] private float _gravityForce;
+    [SerializeField] private float _gravityForceTarget;
+
+    // global gravity control(mouse wheel)
+    [SerializeField] private float _mouseInputWheel;
+    [SerializeField] private float _wheelInputThreshold;
+    [SerializeField] private float _gravityForceHigh;
+    [SerializeField] private float _gravityForceLow;
+    private bool _isGravityLow;
+    [SerializeField] private float _energyLow;
+    
 
     void Start()
     {
         _camera = GameObject.Find("PlayerCamera").transform;
+        _rigidbody = GetComponent<Rigidbody>();
         _isAlive = true;
         _bulletLeft = _maxBullet;
         _isShootable = true;
         _isReloading = false;
         _isTargetable = true;
+        _energy = _maxEnergy;
+        _isGravityLow = false;
     }
-
+    private void FixedUpdate() {
+        if(_isGravityLow) {
+            _rigidbody.AddForce(Vector3.down * _gravityForceLow, ForceMode.Impulse);
+        }
+    }
     void Update()
     {
+        _energyBar.fillAmount = _energy/_maxEnergy;
         // left click event
         if(Input.GetButtonDown("Fire1")) {
             Fire();
@@ -47,8 +77,8 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyDown(_reloadKey)) {
             Reload();
         }
-
-        
+        HandleWheelInput();
+        UpdateEnergy();
     }
 
     private void Fire() {
@@ -100,7 +130,7 @@ public class PlayerController : MonoBehaviour
         if(Physics.Raycast(_camera.position, _camera.transform.forward, out hit)) {
             if(rigid = hit.collider.gameObject.GetComponent<Rigidbody>()) {
                 Debug.Log("TargetGravity: target detected");
-                rigid.AddForce(Vector3.down * rigid.mass * _gravityForce, ForceMode.Impulse);
+                rigid.AddForce(Vector3.down * rigid.mass * _gravityForceTarget, ForceMode.Impulse);
                 // hit.collider.gameObject.GetComponent<EnemyController>().OnHit();
             }
         }
@@ -111,6 +141,31 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(_targetGravityCooldown);
         if(!_isTargetable)
             _isTargetable = true;
+    }
+
+    private void HandleWheelInput() {
+        _mouseInputWheel = Input.GetAxis("Mouse ScrollWheel");
+        if(_mouseInputWheel > _wheelInputThreshold) {
+            _isGravityLow = true;
+        } else if(_mouseInputWheel < -_wheelInputThreshold) {
+            if(_isGravityLow) {
+                _isGravityLow = false;
+            }
+            // TODO: gravity high mechanism
+        }
+    }
+
+    private void UpdateEnergy() {
+        if(_isGravityLow) {
+            _energy -= _energyLow * Time.deltaTime;
+            if(_energy < 0) {
+                _energy = 0;
+                _isGravityLow = false;
+            }
+        } else {
+            _energy += _energyRecover * Time.deltaTime;
+            if(_energy > _maxEnergy) _energy = _maxEnergy;
+        }
     }
 
     public void OnHit() {
