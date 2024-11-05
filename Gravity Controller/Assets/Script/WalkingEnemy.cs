3175c0b6b5fd1c99949d79b3bc06c07bfefcc986
KimGwangmin <gwangmin@snu.ac.kr>
2024-11-05 16:06:59 +0900

    fix: code review & modify
    보행형 적 코드를 전체적으로 리뷰하고, convention 통일 및 일부 매커니즘을 조정했습니다.
    유의미한 변경은 주석으로도 남겨뒀습니다.

--- Gravity Controller/Assets/Script/WalkingEnemy.cs
+++ Gravity Controller/Assets/Script/WalkingEnemy.cs
@@ -4,141 +4,128 @@ using UnityEngine;
 
 public class WalkingEnemy : MonoBehaviour
 {
-	[SerializeField] private GameObject _player;
-	[SerializeField] private float _speed;
-	[SerializeField] private float _attackRange;
-	[SerializeField] private float _followingRange;
-	[SerializeField] private float _wanderRange;
-	[SerializeField] private float _changeDirectionInterval;
-	[SerializeField] private float _obstacleDetectionRange = 200f;
-
 	private Animator _animator;
-	private Vector3 _spawnPoint;
-	private Vector3 _currentDirection;
-	private float _timer;
-	private bool _isFollowingPlayer = false;
-	private bool _isAttacking = false;
 
-	void Start()
-	{
+	[Header("Wander")]
+	[SerializeField] private float _speedWander;
+	[SerializeField] private float _rangeWander;
+	[SerializeField] private float _changeDirectionInterval;
+	[SerializeField] private float _obstacleDetectionRange;
+
+	[Header("Chase")]
+	[SerializeField] private GameObject _player;
+	[SerializeField] private float _speedChase;
+	[SerializeField] private float _rangeAttack;
+	[SerializeField] private float _rangeChase;
+	private bool _isChase = false;
+	private bool _isAttack = false;
+	
+	
+	private Vector3 _spawnPoint;
+	[SerializeField] private Vector3 _currentDirection;
+	private float _timer;
+	
+
+	void Start() {
 		_animator = GetComponent<Animator>();
 		_spawnPoint = transform.position;
 		SetRandomDirection();
 		_timer = _changeDirectionInterval;
 	}
 
-	void Update()
-	{
+	// fix: 구조를 조금 더 깔끔하게 변경
+	void Update() {
 		float distanceToPlayer = Vector3.Distance(transform.position, _player.transform.position);
 
-		if (!_isFollowingPlayer && distanceToPlayer < _followingRange)
-		{
-			_isFollowingPlayer = true;
-			_animator.SetBool("FollowPlayer", true);
-			_speed += 50;
-		}
-
-		if (_isFollowingPlayer && distanceToPlayer < _attackRange)
-		{
-			AttackPlayer();
-		}
-		else if (_isFollowingPlayer && distanceToPlayer >= _attackRange)
-		{
-			FollowPlayer();
-		}
-		else if (!_isFollowingPlayer)
-		{
-			Wander();
-		}
-	}
-
-	private void FollowPlayer()
-	{
-		if (_isAttacking) return;
-
-		Vector3 direction = (_player.transform.position - transform.position).normalized;
-		transform.position += direction * _speed * Time.deltaTime;
-		transform.LookAt(_player.transform.position);
-	}
-
-	private void AttackPlayer()
-	{
-		if (_isAttacking) return;
-
-		_isAttacking = true;
-		_animator.SetBool("AttackPlayer", true);
-
-		Invoke("ResetAttack", 1f);
-	}
-
-	private void ResetAttack()
-	{
-		_isAttacking = false;
-		_animator.SetBool("AttackPlayer", false);
-	}
-
-	public void AttackHitCheck()
-	{
-		float distanceToPlayer = Vector3.Distance(transform.position, _player.transform.position);
-
-		if (distanceToPlayer <= _attackRange)
-		{
-			Debug.Log("Attack Successful");
-
-			PlayerMovement playerController = _player.GetComponent<PlayerMovement>();
-			if (playerController != null)
-			{
-				// playerController.OnHit(); 
+		if(!_isChase) {
+			if(distanceToPlayer < _rangeChase) { // player firstly detected
+				_isChase = true;
+				_animator.SetBool("FollowPlayer", true);
+			} else { // player not detected
+				Wander();
+			}
+		} else { // player detected
+			if(distanceToPlayer < _rangeAttack) { // if close enough
+				Attack();
+			} else {
+				Chase();
 			}
 		}
-		else
-		{
-			Debug.Log("Attack Fail");
-		}
 	}
 
-	private void Wander()
-	{
+	private void Wander() {
 		_timer += Time.deltaTime;
-		if (_timer >= _changeDirectionInterval)
-		{
+		if (_timer > _changeDirectionInterval) {
+			// Issue: interval에도 약간의 랜덤성을 주면 좋을 것 같음
+			SetRandomDirection();
+			_timer = 0f;
+		} else if (Vector3.Distance(_spawnPoint, transform.position) > _rangeWander) {
+			// 일정 범위를 벗어나지 못하도록
+			_currentDirection = (_spawnPoint - transform.position).normalized;
+			_timer = 0f;
+		} else if (Physics.Raycast(transform.position, _currentDirection, _obstacleDetectionRange)) {
+			// 장애물을 만나면 다른 방향으로
 			SetRandomDirection();
 			_timer = 0f;
 		}
 
-		if (Vector3.Distance(_spawnPoint, transform.position) > _wanderRange)
-		{
-			Vector3 directionToSpawn = (_spawnPoint - transform.position).normalized;
-			_currentDirection = directionToSpawn;
-		}
-
-		if (Physics.Raycast(transform.position, _currentDirection, _obstacleDetectionRange))
-		{
-			SetRandomDirection();
-		}
-		else
-		{
-			transform.Translate(_currentDirection * _speed * Time.deltaTime, Space.World);
-
-			if (_currentDirection != Vector3.zero)
-			{
-				Quaternion targetRotation = Quaternion.LookRotation(_currentDirection);
-				transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * _speed);
-			}
-		}
+		// rotate
+		Quaternion targetRotation = Quaternion.LookRotation(_currentDirection);
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * _speedWander);
+		// move
+		transform.Translate(Vector3.forward * _speedWander * Time.deltaTime);
 	}
 
-	private void SetRandomDirection()
-	{
+	private void SetRandomDirection() {
 		float angle = Random.Range(0, 360);
 		_currentDirection = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), 0, Mathf.Sin(angle * Mathf.Deg2Rad)).normalized;
 	}
 
-	private void OnDrawGizmosSelected()
-	{
+	// fix: isAttack 확인 필요 없을 것으로 보여 삭제
+	// fix: 플레이어의 높낮이의 변화가 있으면 문제가 생길 것 같아서 수정
+	private void Chase() {
+		Vector3 direction = Vector3.Scale(_player.transform.position - transform.position, new Vector3(1, 0, 1)).normalized;
+		transform.position += direction * _speedChase * Time.deltaTime;
+		transform.LookAt(_player.transform.position);
+	}
+
+	private void Attack() {
+		if (_isAttack) {
+			return;
+		}
+
+		_isAttack = true;
+		_animator.SetBool("AttackPlayer", true);
+
+		StartCoroutine(ResetAttack());
+	}
+
+	// fix: Invoke 대신 Coroutine 사용하는 구조로 변경
+	IEnumerator ResetAttack() {
+		yield return new WaitForSeconds(1f);
+
+		_isAttack = false;
+		_animator.SetBool("AttackPlayer", false);
+	}
+
+	// fix: player controller가 유효한지 검사하는 과정 삭제
+	public void AttackHitCheck() {
+		float distanceToPlayer = Vector3.Distance(transform.position, _player.transform.position);
+
+		if (distanceToPlayer < _rangeAttack) {
+			Debug.Log("Attack Successful");
+			_player.GetComponent<PlayerController>().OnHit();
+		} else {
+			Debug.Log("Attack Fail");
+		}
+	}
+
+	private void OnDrawGizmosSelected() {
 		Gizmos.color = Color.red;
 		Gizmos.DrawRay(transform.position, _currentDirection * _obstacleDetectionRange);
 		Gizmos.color = Color.green;
-		Gizmos.DrawWireSphere(_spawnPoint, _wanderRange);
+		Gizmos.DrawWireSphere(_spawnPoint, _rangeWander);
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere(transform.position, _rangeChase);
 	}
 }
