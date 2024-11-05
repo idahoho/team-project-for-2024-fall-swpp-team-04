aa27b237a96e943abdfe2efde05cc798de989d71
KimGwangmin <gwangmin@snu.ac.kr>
2024-11-05 16:07:44 +0900

    fix: code review
    불필요한 부분을 삭제했습니다.

--- Gravity Controller/Assets/Script/EnemyController.cs
+++ Gravity Controller/Assets/Script/EnemyController.cs
@@ -5,17 +5,6 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
 	[SerializeField] private int _hp;
-	// Start is called before the first frame update
-	void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 	
 	public void OnHit()
 	{
