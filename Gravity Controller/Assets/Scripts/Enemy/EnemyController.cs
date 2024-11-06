dc9890681833cad5949fe57d2d154af015702f69
KimGwangmin <gwangmin@snu.ac.kr>
2024-11-06 16:50:36 +0900

    fix: enemycontroller 위치 변경
    스크립트 디렉토리로 위치 변경(scripts/enemy)

--- Gravity Controller/Assets/Scripts/Enemy/EnemyController.cs
+++ Gravity Controller/Assets/Scripts/Enemy/EnemyController.cs
@@ -6,11 +6,8 @@ public class EnemyController : MonoBehaviour
 {
 	[SerializeField] private int _hp;
 	
-	public void OnHit()
-	{
-		_hp--;
-		if (_hp <= 0)
-		{
+	public void OnHit() {
+		if (--_hp <= 0) {
 			GameManager.Instance.UnregisterEnemy(gameObject);
 			Destroy(gameObject);
 		}
