dd4236a37c282f533290aa2f43681ef4b501809c
seungminBae <136676026+seungminBae@users.noreply.github.com>
2024-11-23 21:25:30 +0900

    feat: update WalkingEnemy
    EnemyState에 맞추어 코드를 수정했습니다. 전방에 장애물이 있거나 빗면 위에 올라설 경우 움직임이 매우 부자연스러워지므로 맵 디자인 시 주의가 필요합니다.

--- Gravity Controller/Assets/Scripts/Enemy/WalkingEnemy.cs
+++ Gravity Controller/Assets/Scripts/Enemy/WalkingEnemy.cs
@@ -7,12 +7,17 @@ public class WalkingEnemy : MonoBehaviour, IEnemy
 	private Animator _animator;
 	[Header("Target")]
 	private GameObject _player;
+	[SerializeField] private float _heightOffset;
+	[SerializeField] private float _playerHeightOffset;
 
 	[Header("Wander")]
 	[SerializeField] private float _wanderSpeed;
 	[SerializeField] private float _wanderRange;
+	[SerializeField] private float _rotationSpeed;
 	[SerializeField] private float _changeDirectionInterval;
 	[SerializeField] private float _obstacleDetectionRange;
+	[SerializeField] private float _changeDirectionIntervalMin;
+	[SerializeField] private float _changeDirectionIntervalMax;
 	private Vector3 _spawnPoint;
 	private Vector3 _currentDirection;
 	private float _timer = 0f;
@@ -24,6 +29,11 @@ public class WalkingEnemy : MonoBehaviour, IEnemy
 	[SerializeField] private float _attackHitRange;
 	private bool _isChasing = false;
 	private bool _isAttacking = false;
+
+	[SerializeField] private float _sightRange;
+	[SerializeField] private float _awarenessCoolDown;
+	private float _awarenessCoolDownTimer;
+	private Vector3 _lastSeenPosition;
 
 	[SerializeField] private int _maxHp;
 	private int _hp;
@@ -40,20 +50,25 @@ public class WalkingEnemy : MonoBehaviour, IEnemy
 	void Start() {
 		_player = GameObject.Find("Player");
 
-		_animator = GetComponent<Animator>();
+		_animator = transform.GetChild(0).GetComponent<Animator>();
 		_spawnPoint = transform.position;
+
 		SetRandomDirection();
+		BeforeWander();
 	}
 
 	// fix: 구조를 조금 더 깔끔하게 변경
+	/*
 	void Update() {
 		float distanceToPlayer = Vector3.Distance(transform.position, _player.transform.position);
 
 		if(!_isChasing) {
 			if(distanceToPlayer < _chaseRange) { // player firstly detected
 				_isChasing = true;
-				_animator.SetBool("FollowPlayer", true);
+				_animator.SetBool("IsRunning", true);
 			} else { // player not detected
+				_isChasing = false;
+				_animator.SetBool("IsWalking", true);
 				Wander();
 			}
 		} else { // player detected
@@ -64,6 +79,100 @@ public class WalkingEnemy : MonoBehaviour, IEnemy
 			}
 		}
 	}
+	*/
+
+	private void FixedUpdate()
+	{
+		var relativePosition = _player.transform.position + _playerHeightOffset * new Vector3(0, 1, 0) - transform.position - _heightOffset * new Vector3(0, 1, 0);
+		float distanceHorizontal = Vector3.Scale(relativePosition, new Vector3(1, 0, 1)).magnitude;
+		float distanceVertical = transform.position.y - _player.transform.position.y;
+		RaycastHit hit;
+		bool playerInSight = false;
+
+		_awarenessCoolDownTimer -= Time.fixedDeltaTime;
+
+		if (Physics.Raycast(transform.position + _heightOffset * new Vector3(0,1,0), Vector3.Scale(relativePosition, new Vector3(1, 0, 1)), out hit, _sightRange))
+		{
+			playerInSight = hit.collider.gameObject.CompareTag("Player");
+			if (playerInSight)
+			{
+				_awarenessCoolDownTimer = _awarenessCoolDown;
+				_lastSeenPosition = _player.transform.position;
+			}
+			Debug.Log("Hit:" + hit.collider.name);
+		}
+
+		switch (State)
+		{
+			case EnemyState.Idle:
+				if ((distanceHorizontal <= _chaseRange) && playerInSight)
+				//if (playerInSight)
+					{
+					// Idle -> Aware
+					State = EnemyState.Aware;
+					_animator.SetBool("IsRunning", true);
+					_animator.SetBool("IsWalking", false);
+					Chase();
+					break;
+				}
+				Wander();
+				break;
+			case EnemyState.Aware:
+				if (!playerInSight || !(distanceHorizontal <= _chaseRange))
+				//if (!playerInSight)
+					{
+					// cannot see the player || player not in range
+					if (_awarenessCoolDownTimer > 0)
+					{
+						// Aware -> Follow
+						State = EnemyState.Follow;
+						Follow();
+						break;
+					}
+					// Aware -> Idle
+					_animator.SetBool("IsRunning", false);
+					State = EnemyState.Idle;
+					BeforeWander();
+					Wander();
+					break;
+				}
+
+				Chase();
+				break;
+			case EnemyState.Follow:
+				if (_awarenessCoolDownTimer <= 0 || _lastSeenPosition == transform.position)
+				{
+					// time's up || lost player
+					// Follow -> Idle
+					_animator.SetBool("IsRunning", false);
+					State = EnemyState.Idle;
+					BeforeWander();
+					Wander();
+					break;
+				}
+				if (distanceHorizontal <= _chaseRange)
+				//if (playerInSight)
+					{
+					// gotcha
+					// Follow -> Aware
+					State = EnemyState.Aware;
+					Chase();
+					break;
+				}
+
+				Follow();
+				break;
+		}
+
+		Debug.Log(State);
+	}
+
+	private void BeforeWander()
+	{
+		_animator.SetBool("IsWalking", true);
+		SetRandomInterval();
+		_timer = 0f;
+	}
 
 	private void Wander() {
 		_timer += Time.deltaTime;
@@ -71,7 +180,24 @@ public class WalkingEnemy : MonoBehaviour, IEnemy
 			// Issue: interval에도 약간의 랜덤성을 주면 좋을 것 같음
 			SetRandomDirection();
 			_timer = 0f;
-		} else if(Vector3.Distance(_spawnPoint, transform.position) > _wanderRange) {
+		}
+		else if (_timer > _changeDirectionInterval) {
+			// phase transition: moving <-> rotating
+			if (Vector3.Distance(_spawnPoint, transform.position) > _wanderRange)
+			{
+				// too far from origin
+				_currentDirection = Vector3.Scale(_spawnPoint - transform.position, new Vector3(1, 0, 1)).normalized;
+			}
+			else
+			{
+				// okay to go
+				SetRandomDirection();
+			}
+			SetRandomInterval();
+			_timer = 0f;
+		}
+		/*
+		else if(Vector3.Distance(_spawnPoint, transform.position) > _wanderRange) {
 			// 일정 범위를 벗어나지 못하도록
 			_currentDirection = (_spawnPoint - transform.position).normalized;
 			_timer = 0f;
@@ -80,27 +206,48 @@ public class WalkingEnemy : MonoBehaviour, IEnemy
 			SetRandomDirection();
 			_timer = 0f;
 		}
+		*/
 
 		// rotate
 		Quaternion targetRotation = Quaternion.LookRotation(_currentDirection);
-		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * _wanderSpeed);
+		Quaternion targetRotationHorizontal = Quaternion.LookRotation(Vector3.Scale(_currentDirection, new Vector3(1, 0, 1)));
+		var tempRotation = Quaternion.Slerp(transform.rotation, targetRotationHorizontal, Time.fixedDeltaTime * _rotationSpeed);
+		transform.rotation = tempRotation;
+
 		// move
-		transform.Translate(Vector3.forward * Time.deltaTime * _wanderSpeed);
+		transform.Translate(Vector3.forward * Time.fixedDeltaTime * _wanderSpeed);
 	}
 
-	private void SetRandomDirection() {
+	private void SetRandomInterval()
+	{
+		_changeDirectionInterval = Random.Range(_changeDirectionIntervalMin, _changeDirectionIntervalMax);
+	}
+
+	private void SetRandomDirection()
+	{
 		float angle = Random.Range(0, 360);
 		_currentDirection = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), 0, Mathf.Sin(angle * Mathf.Deg2Rad)).normalized;
 	}
 
 	// fix: 플레이어의 높낮이의 변화가 있으면 문제가 생길 것 같아서 수정
 	private void Chase() {
+		// not to be confused with FlyingEnemy.ChasePlayer(); this method is for EnemyState.Aware
 		if(_isAttacking) {
 			return;
 		}
+
+		float distanceToPlayer = Vector3.Scale(transform.position - _player.transform.position, new Vector3(1, 0, 1)).magnitude;
+
+		if (distanceToPlayer < _attackRange)
+		{
+			Attack();
+			return;
+		}
+
 		Vector3 direction = Vector3.Scale(_player.transform.position - transform.position, new Vector3(1, 0, 1)).normalized;
-		transform.position += direction * _chaseSpeed * Time.deltaTime;
-		transform.LookAt(_player.transform.position);
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.fixedDeltaTime * _rotationSpeed);
+		transform.Translate(Vector3.forward * _chaseSpeed * Time.deltaTime);
+		_spawnPoint = transform.position;
 	}
 
 	private void Attack() {
@@ -109,7 +256,7 @@ public class WalkingEnemy : MonoBehaviour, IEnemy
 		}
 
 		_isAttacking = true;
-		_animator.SetBool("AttackPlayer", true);
+		_animator.SetBool("Attack", true);
 
 		StartCoroutine(ResetAttack());
 	}
@@ -119,7 +266,7 @@ public class WalkingEnemy : MonoBehaviour, IEnemy
 		yield return new WaitForSeconds(1f);
 
 		_isAttacking = false;
-		_animator.SetBool("AttackPlayer", false);
+		_animator.SetBool("Attack", false);
 	}
 
 	// fix: player controller가 유효한지 검사하는 과정 삭제
@@ -132,6 +279,30 @@ public class WalkingEnemy : MonoBehaviour, IEnemy
 		} else {
 			Debug.Log("Attack Fail");
 		}
+	}
+
+	private void Follow()
+	{
+		Vector3 directionToPlayer = _lastSeenPosition - transform.position;
+		Vector3 directionToPlayerHorizontal = Vector3.Scale(directionToPlayer, new Vector3(1, 0, 1));
+
+		// rotate
+		Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+		Quaternion targetRotationHorizontal = Quaternion.LookRotation(directionToPlayerHorizontal);
+		var tempRotation = Quaternion.Slerp(transform.rotation, targetRotationHorizontal, Time.fixedDeltaTime * _rotationSpeed);
+		transform.rotation = tempRotation;
+
+		// move
+		Vector3 dir = Vector3.zero;
+		Vector3 target = Vector3.zero;
+
+
+		target += directionToPlayerHorizontal.normalized;
+		
+		dir = target.normalized * Time.fixedDeltaTime * _chaseSpeed;
+
+		transform.Translate(Quaternion.Inverse(transform.rotation) * dir);
+		_spawnPoint = transform.position;
 	}
 
 	private void OnDrawGizmosSelected() {
