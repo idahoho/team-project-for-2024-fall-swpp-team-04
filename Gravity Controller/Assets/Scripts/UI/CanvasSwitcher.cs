c64c355bcc3e9d9b34ec23492e524c04030f64d6
seungminBae <136676026+seungminBae@users.noreply.github.com>
2024-12-08 22:48:24 +0900

    feat: Save settings
    환경설정 데이터를 저장합니다.
    
    환경설정 탭에 진입할 때, 저장해 둔 파일을 찾아 값을 적용합니다. 환경설정 탭을 닫을 때, 파일에 변경점을 덮어씁니다.
    
    슬라이더의 값은 0부터 100까지의 퍼센트로 나타냅니다. 기본값은 임시로 50%로 설정해 두었습니다.

--- Gravity Controller/Assets/Scripts/UI/CanvasSwitcher.cs
+++ Gravity Controller/Assets/Scripts/UI/CanvasSwitcher.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CanvasSwitcher : MonoBehaviour
 {
@@ -8,6 +10,9 @@ public class CanvasSwitcher : MonoBehaviour
 	public CanvasGroup newCanvas; // 활성화할 새로운 캔버스
 	public float fadeDuration = 0.5f; // 페이드 효과 지속 시간
 	public GameObject[] panels; // 버튼과 연결된 패널들을 배열로 저장.
+
+	[SerializeField] private GameObject[] _sliders;
+	[SerializeField] private float[] _defaultValues;
 
 	// 버튼 클릭 시 호출되는 함수
 	public void ShowPanel(int index)
@@ -28,12 +33,16 @@ public class CanvasSwitcher : MonoBehaviour
 	// 캔버스를 스위치하는 첫 번째 버튼 함수
 	public void SwitchToNewCanvas()
 	{
+		// Load settings
+		LoadSettingsSave();
 		StartCoroutine(FadeOutAndIn(currentCanvas, newCanvas));
 	}
 
 	// 캔버스를 스위치하는 두 번째 버튼 함수
 	public void SwitchToCurrentCanvas()
 	{
+		// Save settings
+		SaveSettingsSave();
 		StartCoroutine(FadeOutAndIn(newCanvas, currentCanvas));
 	}
 
@@ -83,5 +92,48 @@ public class CanvasSwitcher : MonoBehaviour
 		}
 
 		canvasGroup.alpha = 1f; // 페이드 인 완료
+	}
+
+	private void LoadSettingsSave()
+	{
+		string json;
+		if (!FileManager.LoadFromFile("settings.dat", out json))
+		{
+			// No file; Set to default
+			SetSliders(_defaultValues);
+			return;
+		}
+		var settingsSave = SettingsSave.Restore(json);
+		if(settingsSave == null)
+		{
+			// Invalid file; Set to default
+			Debug.Log("Invalid file: " + "settings.dat");
+			SetSliders(_defaultValues);
+			return;
+		}
+		var values = new float[3];
+		values[0] = settingsSave.backGroundVolume;
+		values[1] = settingsSave.effectVolume;
+		values[2] = settingsSave.sensitivity;
+		SetSliders(values);
+	}
+	private void SaveSettingsSave()
+	{
+		var save = new SettingsSave();
+		save.backGroundVolume = _sliders[0].GetComponent<Slider>().value;
+		save.effectVolume = _sliders[1].GetComponent<Slider>().value;
+		save.sensitivity = _sliders[2].GetComponent<Slider>().value;
+
+		FileManager.WriteToFile("settings.dat", JsonUtility.ToJson(save));
+	}
+
+	private void SetSliders(float[] values)
+	{
+		for(int i=0; i< values.Length; i++)
+		{
+			if (values[i] < 0) values[i] = 0;
+			if (values[i] > 100) values[i] = 100;
+			_sliders[i].GetComponent<Slider>().value = values[i];
+		}
 	}
 }
